df4f236549d69b730ce8135b487473b283c7858d
softengg.roshan@gmail.com <Rosh@n#123>
2025-02-22 23:17:06 +0530

    initial Commit

--- app/src/main/java/com/roshan/sample/MainActivity.kt
+++ app/src/main/java/com/roshan/sample/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.roshan.sample.presentation.screens.listingScreen
+import com.roshan.sample.presentation.screens.ListingScreen
 import com.roshan.sample.ui.theme.ComposePOCTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    listingScreen()
+                    ListingScreen()
                 }
             }
         }
